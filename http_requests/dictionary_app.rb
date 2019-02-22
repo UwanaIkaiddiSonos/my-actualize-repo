df1b83f0c2c0f895a58264ae693191bba1f459b6
Uwana <uwikaiddi@gmail.com>
2019-02-21 21:29:03 -0600

    Add additional definitions

--- http_requests/dictionary_app.rb
+++ http_requests/dictionary_app.rb
@@ -5,8 +5,16 @@ user_word = gets.chomp
 
 response = HTTP.get("https://api.wordnik.com/v4/word.json/#{user_word}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=#{key}")
 
+define = response.parse
 definition = response.parse[0]["text"]
 
-#puts response.parse.class
-#p response.parse
-puts "Definition: #{definition}"
+# #puts response.parse.class
+# #p response.parse
+# puts "Definition: #{definition}"
+
+i = 0
+define.length.times do
+  puts "Definition #{i + 1}"
+  p define[i]["text"]
+  i += 1
+end
