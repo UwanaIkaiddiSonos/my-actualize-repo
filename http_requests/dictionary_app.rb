2cf7986107883008f1f6d6744fc2bcda3691c297
Uwana <uwikaiddi@gmail.com>
2019-02-24 09:42:59 -0600

    Add Rails folder

--- http_requests/dictionary_app.rb
+++ http_requests/dictionary_app.rb
@@ -1,5 +1,5 @@
 require 'http'
-key = ""
+key = "64e90a58d89a8e7f3f000001fe809d0cd55d32cb45b9f117e"
 puts "Enter a word:"
 user_word = gets.chomp
 
@@ -13,7 +13,11 @@ definition = response.parse[0]["text"]
 # puts "Definition: #{definition}"
 
 i = 0
+puts "Top definition:"
+puts define[0]
+
 define.length.times do
+  puts "Other definitions"
   puts "Definition #{i + 1}"
   p define[i]["text"]
   i += 1
